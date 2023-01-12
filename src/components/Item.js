b8c28301ac3a4a6d92e928377d9286d3b0b153fd
JohnOkari <johnnokari@gmail.com>
2023-01-12 18:22:22 +0300

    Done with assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [isClick, setOnchange] = useState(false);
+
+  function handleChange(){
+    setOnchange((isClick) => !isClick);
+  }
   return (
-    <li className="">
+    <li className={ isClick ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleChange}>{ isClick ?"Remove from Cart" :"Add to Cart" }</button>
     </li>
   );
 }
